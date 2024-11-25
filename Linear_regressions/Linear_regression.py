72897bff24c4005f4a3858efaba923d807eeb242
Dubrovin Nikita <146526027+Mizoroki-Heck@users.noreply.github.com>
2024-11-25 17:39:33 +0800

    Rewrote the class, and did not test much on the data

--- Linear_regressions/Linear_regression.py
+++ Linear_regressions/Linear_regression.py
@@ -2,13 +2,16 @@ import numpy as np
 
 class Analytical_Linear_Regression:
 
+    def __init__(self):
+        pass
+
     def fit(self, X_train, y_train):
-        X_train = np.insert(X_train,0, 1, axis=1)
-        self.weights = np.linalg.inv(X_train.T.dot(X_train)).dot(X_train.T).dot(y_train)
-        self.bias = self.weights[0]
-        self.weights = self.weights[1]
-        
+        X_train = np.insert(X_train,0,1,axis=1)
+        self.wieghts = np.inv(X_train.T.dot(X_train)).dot(X_train.T).dot(y_train)
+
+
     def predict(self, X_test):
-        y_pred = X_test.dot(self.weights) + self.bias
-        return y_pred
-        
+        X_test = np.insert(X_test, 0, 1, axis=1)
+        return X_test.dot(self.wieghts)
+
+
